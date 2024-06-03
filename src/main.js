2045953c8ed962001d72e879746b3e16e1cc14fe
dewang <2002thapa@gmail.com>
2024-06-03 23:11:31 +0530

    updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const openmenu = document.querySelector("#openmenu");
 const closemenu = document.querySelector("#closemenu");
 const menuoptions = document.querySelector("#menu");
-var show = 0;
+const scrollup =  document.querySelector(".moveup")
 openmenu.addEventListener("click", () => {
   menuoptions.classList.remove("hidden");
 });
@@ -9,35 +9,40 @@ closemenu.addEventListener("mousedown", () => {
   menuoptions.classList.add("hidden");
 });
 const category = document.querySelectorAll(".category button");
-const all = document.querySelectorAll(".showall")
-const snak = document.querySelectorAll(".snak")
-const food = document.querySelectorAll(".food")
-const beverages = document.querySelectorAll(".beverages")
+const all = document.querySelectorAll(".showall");
+const snak = document.querySelectorAll(".snak");
+const food = document.querySelectorAll(".food");
+const beverages = document.querySelectorAll(".beverages");
 category.forEach((elem) => {
   elem.addEventListener("click", () => {
     category.forEach((elem) => {
       elem.classList.remove("active");
     });
     elem.classList.add("active");
-    const tabval=elem.getAttribute("data")
-    all.forEach((elem)=>{
-      elem.style.display="none"
-    })
+    const tabval = elem.getAttribute("data");
+    all.forEach((elem) => {
+      elem.style.display = "none";
+    });
 
-   if(tabval=="food"){
-    food.forEach(elem=>elem.style.display="block")}
-   else if(tabval=="snaks"){
-    snak.forEach(elem=>elem.style.display="block")}
-   else if(tabval=="beverage"){
-    beverages.forEach(elem=>elem.style.display="block")}
-    else{
-        all.forEach((elem)=>{
-            elem.style.display="block"
-          })
+    if (tabval == "food") {
+      food.forEach((elem) => (elem.style.display = "block"));
+    } else if (tabval == "snaks") {
+      snak.forEach((elem) => (elem.style.display = "block"));
+    } else if (tabval == "beverage") {
+      beverages.forEach((elem) => (elem.style.display = "block"));
+    } else {
+      all.forEach((elem) => {
+        elem.style.display = "block";
+      });
     }
-    
-
-
   });
-
 });
+
+const scroll=()=>{
+  if(this.screenY>=250){
+    scrollup.style.display="block";
+    
+  }
+}
+
+ window.addEventListener("scroll",scroll)
